423babe2131710a18d842e6e97046da402f8e627
Zach Peterson <zachpeterson13@icloud.com>
2023-03-18 14:58:51 -0600

    Initial Commit

---
+++ Workout Tracker/Workout_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_TrackerApp.swift
+//  Workout Tracker
+//
+//  Created by Zach Peterson on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Workout_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
